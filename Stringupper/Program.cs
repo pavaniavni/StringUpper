94528318cf6a54986c0d4ccf7844c0efcccf8c0f
pavani <pavaniavni456@gmail.com>
2022-02-09 15:42:31 +0530

    added stringupper

---
+++ Stringupper/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+
+namespace StringUpper
+{
+    public class StringUpp
+    {
+        
+            static void Main(string[] args)
+            {
+                List<string> mylist = test(new List<string>(new string[] { Console.ReadLine() }));
+                foreach (var i in mylist)
+                {
+                    Console.Write(i.ToString() + " ");
+                }
+            }
+            public static List<string> test(List<string> str)
+            {
+                IEnumerable<string> s = str.Select(x => x.ToUpper());
+                return s.ToList();
+            }
+    }
+ }
+
+
